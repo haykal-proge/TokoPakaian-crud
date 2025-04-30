56ea8581e677eca2dcb2c4b081369803c25800f7
nadyaatiqah22 <dyatiqah22@gmail.com>
2025-04-30 10:29:07 +0700

    Add caching commands to start.sh for improved performance during Laravel initialization

--- start.sh
+++ start.sh
@@ -6,5 +6,9 @@ php-fpm -D
 # Jalankan Laravel migration
 php artisan migrate --force
 
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
 # Jalankan nginx di foreground
 nginx -g "daemon off;"
